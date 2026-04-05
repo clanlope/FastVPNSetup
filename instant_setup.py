2de4942854304e5c97978a0e7e45de0d4cfede47
Clanlope <32638534+clanlope@users.noreply.github.com>
2026-04-05 17:20:21 +0800

    Update instant_setup.py

--- instant_setup.py
+++ instant_setup.py
@@ -26,6 +26,7 @@ YAML_FILENAME = "config.yaml"
 
 API_BASE = "https://api.vultr.com/v2"
 HEADERS = {"Authorization": f"Bearer {API_KEY}"}
+SCRIPT_URL = "https://github.com/233boy/sing-box/raw/main/install.sh"
 REGION = "icn"  # Seoul, South Korea
 PLAN = "vc2-1c-1gb"  # vc2-1c-1gb $0.007/hr, vhp-1c-1gb $0.008/hr
 OS_ID = 2657  # Ubuntu 22.04 x64
@@ -35,6 +36,7 @@ LABEL = "AA_VPN"
 def timer(func):
     def wrapper(*args, **kwargs):
         start = time.perf_counter()
+        print(f"🕛 Starting at {datetime.now(timezone.utc)}")
         result = func(*args, **kwargs)
         elapsed = time.perf_counter() - start
         print(f"🕛 Executed in {elapsed:.2f}s")
@@ -93,9 +95,7 @@ def _wait_instance(instance_id):
             print(f"🟢 IP: {info['main_ip']}")
             return info["main_ip"]
         iteration += 1
-        if iteration > 10:
-            raise TimeoutError("🔴 Instance did not become active in time")
-        time.sleep(3)
+        time.sleep(5)
 
 
 def _run_cmd(client, cmd):
@@ -104,34 +104,31 @@ def _run_cmd(client, cmd):
     return stdout.read().decode(), stderr.read().decode()
 
 
-def _ssh_connect(instance_id, password, attempts=6):
+def _ssh_connect(instance_id, password, attempts=10):
     address = _wait_instance(instance_id)
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     for attempt in range(1, attempts + 1):
         try:
-            client = paramiko.SSHClient()
-            client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            client.connect(address, username="root", password=password, timeout=10)
+            time.sleep(12)  # Wait a bit before trying to connect
+            client.connect(address, username="root", password=password)
             print("🟢 SSH connection established")
             break
         except Exception as e:
             print(f"⚪ SSH no response, retrying... ({attempt}/{attempts}): {e}")
+            client.close()
 
     else:
-        print("🔴 Error occurred, try rebooting...")
-        _reboot_instance(instance_id)
-        return _ssh_connect(instance_id, password)
+        print("🔴 Error occurred, try manually...")
+        # _reboot_instance(instance_id)
+        print(f"🟡 Username: root, Password: {password}")
+        raise (Exception("SSH connection failed after multiple attempts"))
 
-    scr = "bash <(wget -qO- https://github.com/233boy/sing-box/raw/main/install.sh)"
-    _run_cmd(client, scr)
+    _run_cmd(client, f"bash <(wget -qO- {SCRIPT_URL})")
     _run_cmd(client, "sb bbr")
 
     out, _ = _run_cmd(client, "sb add ss")
-    match = re.search(r"ss://[^\s]+", out)
-    if not match:
-        raise Exception("❌ SS link not found")
-
-    ss_url = match.group(0)
-    ss_url = ss_url.split("#")[0] if "#" in ss_url else ss_url
+    ss_url = re.search(r"ss://[^\s]+", out).group(0).split("#")[0]
     port = re.search(r":(\d{4,5})", out).group(1)
 
     _run_cmd(client, "ufw --force enable")
@@ -194,9 +191,8 @@ def _create_qr(ss_url):
 
 
 def setup_a_server():
-    for _ in range(3, 0, -1):
-        print(f"⚪ Deploying starts in {_}s...")
-        time.sleep(1)
+    print("⚪ Deploying starts in 3s...")
+    time.sleep(3)
     pwd, ins_id = _deploy_instance(REGION, PLAN, OS_ID, LABEL)
     ss = _ssh_connect(ins_id, pwd)
     print(f"🟢 {ss}")
@@ -220,11 +216,11 @@ def check_account():
     res = rea.get("account", {})
     wd = [
         "name",
-        "email",
+        # "email",
         "balance",
         "pending_charges",
-        "last_payment_amount",
-        "last_payment_date",
+        # "last_payment_amount",
+        # "last_payment_date",
     ]
     filtered = {k: res.get(k) for k in wd if k in res}
     print("🔍 Account Info:")
